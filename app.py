2cafa624dd8e82054a5208ce7c156f024d0bb109
Hsiaoming Yang <me@lepture.com>
2013-11-20 23:30:46 +0800

    Add Client.

--- app.py
+++ app.py
@@ -3,7 +3,9 @@
 from flask import Flask
 from flask import session, request
 from flask import render_template, redirect
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import gen_salt
 
 
 app = Flask(__name__, template_folder='templates')
@@ -18,6 +20,33 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), unique=True)
+
+
+class Client(db.Model):
+    client_key = db.Column(db.String(40), primary_key=True)
+    client_secret = db.Column(db.String(55), index=True, nullable=False)
+
+    # creator of the client
+    user_id = db.Column(db.ForeignKey('user.id'))
+    user = db.relationship('User')
+    _realms = db.Column(db.Text)
+    _redirect_uris = db.Column(db.Text)
+
+    @property
+    def redirect_uris(self):
+        if self._redirect_uris:
+            return self._redirect_uris.split()
+        return []
+
+    @property
+    def default_redirect_uri(self):
+        return self.redirect_uris[0]
+
+    @property
+    def default_realms(self):
+        if self._realms:
+            return self._realms.split()
+        return []
 
 
 def current_user():
@@ -40,6 +69,24 @@ def home():
         return redirect('/')
     user = current_user()
     return render_template('home.html', user=user)
+
+
+@app.route('/client')
+def client():
+    user = current_user()
+    if not user:
+        return redirect('/')
+    item = Client(
+        client_key=gen_salt(40),
+        client_secret=gen_salt(50),
+        user_id=user.id,
+    )
+    db.session.add(item)
+    db.session.commit()
+    return jsonify(
+        client_key=item.client_key,
+        client_secret=item.client_secret
+    )
 
 
 if __name__ == '__main__':
